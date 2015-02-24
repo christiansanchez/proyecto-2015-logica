575bde6846d95a0493da2ebe7eed3456e228b345
chjse1987@gmail.com <chjse1987@gmail.com@69f94e97-0af0-e0d7-59d8-1d2504e4d927>
2015-02-24 01:21:29 +0000

    Se agregan figuras en constantes y se insertan en la db, se debe ejecutar solo crearDB.

--- trunk/base_datos/src/db_Juego/CargarDB.java
+++ trunk/base_datos/src/db_Juego/CargarDB.java
@@ -1,10 +1,74 @@
 package db_Juego;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CargarDB {
+	
+	private Connection conn;
+	private ConsultasAgregarCargaDB consultas;
+	
+	public CargarDB(Connection conn){
+		this.conn = conn;
+		this.consultas = new ConsultasAgregarCargaDB();
+	}
+	
+	public void cargarFiguras()throws SQLException{
+		
+		String query = this.consultas.insertFiguras();
+		PreparedStatement pstmt = this.conn.prepareStatement(query);				
+		
+		//figuras
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA1);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA1);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA2);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA2);	
+		pstmt.executeUpdate();
+				
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA3);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA3);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA4);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA4);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA5);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA5);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA6);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA6);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA7);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA7);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_MANGUERA8);				
+		pstmt.setString(2, ConstantesDB.TIPO_MANGUERA8);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_BARCO);				
+		pstmt.setString(2, ConstantesDB.TIPO_BARCO);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_LANCHA2);				
+		pstmt.setString(2, ConstantesDB.TIPO_LANCHA2);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_LANCHA1);				
+		pstmt.setString(2, ConstantesDB.TIPO_LANCHA1);	
+		pstmt.executeUpdate();
+		
+		pstmt.setInt(1, ConstantesDB.ID_LANCHA3);				
+		pstmt.setString(2, ConstantesDB.TIPO_LANCHA3);	
+		pstmt.executeUpdate();	
 
-	
-		//A implementar está noche
-	
+		pstmt.close();	
+	}
 		
 	
 	
--- trunk/base_datos/src/db_Juego/ConstantesDB.java
+++ trunk/base_datos/src/db_Juego/ConstantesDB.java
@@ -5,23 +5,52 @@ public class ConstantesDB {
 			//Para Cargar la Base de datos
 			
 			//Roles:
-			static final int ID_ROL_BARCO = 1;
-			static final int ID_ROL_LANCHA = 2;
+			//static final int ID_ROL_BARCO = 1;
+			//static final int ID_ROL_LANCHA = 2;
 			
 			
 			//Tipos de roles
-			static final String	NOMBRE_ROL_Barco = "BarcoCarguero";
-			static final String	NOMBRE_ROL_LANCHA = "LanchaPirata";
+			//static final String	NOMBRE_ROL_Barco = "BarcoCarguero";
+			//static final String	NOMBRE_ROL_LANCHA = "LanchaPirata";
 				
 			//Estados de las partidas
-			static final int ID_ESTADO_CREADA = 1;
-			static final int ID_ESTADO_ENCURSO = 2;
-			static final int ID_ESTADO_TERMINADA = 3;
-				
-			//NOMBRE DE ESTADOS DE PARTIDA
-			static final String	NOMBRE_ESTADO_CREADA = "Creada";
-			static final String	NOMBRE_ESTADO_ENCURSO = "EnCurso";
-			static final String	NOMBRE_ESTADO_TERMINADA = "Terminada";
+//			static final int ID_ESTADO_CREADA = 1;
+//			static final int ID_ESTADO_ENCURSO = 2;
+//			static final int ID_ESTADO_TERMINADA = 3;
+//				
+//			//NOMBRE DE ESTADOS DE PARTIDA
+//			static final String	NOMBRE_ESTADO_CREADA = "Creada";
+//			static final String	NOMBRE_ESTADO_ENCURSO = "EnCurso";
+//			static final String	NOMBRE_ESTADO_TERMINADA = "Terminada";
+	
+			//ID DE FIGURAS:
+			static final int ID_MANGUERA1 = 1;
+			static final int ID_MANGUERA2 = 2;
+			static final int ID_MANGUERA3 = 3;
+			static final int ID_MANGUERA4 = 4;
+			static final int ID_MANGUERA5 = 5;
+			static final int ID_MANGUERA6 = 6;
+			static final int ID_MANGUERA7 = 7;
+			static final int ID_MANGUERA8 = 8;
+			static final int ID_BARCO = 9;
+			static final int ID_LANCHA1 = 10;
+			static final int ID_LANCHA2 = 11;
+			static final int ID_LANCHA3 = 12;
+			
+			//TIPOS DE FIGURAS:
+			static final String	TIPO_MANGUERA1 = "MANGUERA1";
+			static final String	TIPO_MANGUERA2 = "MANGUERA2";
+			static final String	TIPO_MANGUERA3 = "MANGUERA3";
+			static final String	TIPO_MANGUERA4 = "MANGUERA4";
+			static final String	TIPO_MANGUERA5 = "MANGUERA5";
+			static final String	TIPO_MANGUERA6 = "MANGUERA6";
+			static final String	TIPO_MANGUERA7 = "MANGUERA7";
+			static final String	TIPO_MANGUERA8 = "MANGUERA8";
+			static final String	TIPO_BARCO = "BARCO";
+			static final String	TIPO_LANCHA1 = "LANCHA1";
+			static final String	TIPO_LANCHA2 = "LANCHA2";
+			static final String	TIPO_LANCHA3 = "LANCHA3";
+			
 			
 			
 }
