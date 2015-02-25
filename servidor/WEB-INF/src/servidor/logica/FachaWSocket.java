0d7ab4a5f754b3e09c84b8f7c83ddbb99ab8b369
chjse1987@gmail.com <chjse1987@gmail.com@69f94e97-0af0-e0d7-59d8-1d2504e4d927>
2015-02-25 01:22:09 +0000

    fachada web socket

--- servidor/WEB-INF/src/servidor/logica/FachaWSocket.java
+++ servidor/WEB-INF/src/servidor/logica/FachaWSocket.java
@@ -1,7 +1,9 @@
 package servidor.logica;
 
-import servidor.excepciones.FachadaException;
-import servidor.excepciones.MonitorException;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -13,12 +15,44 @@ import javax.websocket.server.ServerEndpoint;
  * desde el web socket.
  */
 
-@ServerEndpoint("/fachadawsocket")
-public class FachaWSocket extends Fachada{
+import servidor.excepciones.FachadaException;
 
+@ServerEndpoint("/wsocketjuego")
+public class FachaWSocket extends Fachada{
+	
+	private static Set<Session> clients = 
+		    Collections.synchronizedSet(new HashSet<Session>());
+	
 	public FachaWSocket() throws FachadaException {
-		super();
+		Fachada.getInstancia();
 	}
+	
+	@OnMessage
+	public void onMessage(String message, Session session) throws FachadaException{	    
+	    synchronized(clients){
+	      for(Session client : clients){
+	    	  if (!client.equals(session)){
+	    		  try {
+	    			  client.getBasicRemote().sendText(message);
+	    		  }
+	    		  catch (IOException e) {
+	    			  throw new FachadaException(); 
+	    		  }
+	    	  }
+	      }
+	    }
+	    
+	 }
+	
+	@OnOpen
+	public void onOpen (Session session) {
+		clients.add(session);
+	}
+
+	@OnClose
+	public void onClose (Session session) {
+	    clients.remove(session);
+	}		
 	
 //	public boolean setUnirsePartida(String nombrePartida, String rolPartida) throws FachadaException{
 //		nombrePartida = nombrePartida.trim();
